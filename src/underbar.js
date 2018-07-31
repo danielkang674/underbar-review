f3d90489c02dfe4691a7adc0418eee88543135a0
Hack Reactor Student <student@hackreactor.com>
2018-07-31 12:33:15 -0500

    pt1 finished

--- src/underbar.js
+++ src/underbar.js
@@ -7,6 +7,7 @@
   // seem very useful, but remember it--if a function needs to provide an
   // iterator when the user does not pass one in, this will be handy.
   _.identity = function(val) {
+    return val;  
   };
 
   /**
@@ -37,6 +38,13 @@
   // Like first, but for the last elements. If n is undefined, return just the
   // last element.
   _.last = function(array, n) {
+    if(n === 0){
+      return [];
+    }
+    if(n>array.length){
+      return array;
+    }
+    return n === undefined ? array[array.length-1] : array.slice(n-1, array.length);
   };
 
   // Call iterator(value, key, collection) for each element of collection.
@@ -45,6 +53,16 @@
   // Note: _.each does not have a return value, but rather simply runs the
   // iterator function over each item in the input collection.
   _.each = function(collection, iterator) {
+    if(Array.isArray(collection)){
+      for(var i = 0; i < collection.length; i++){
+        iterator(collection[i], i, collection);
+      }
+    }else if(typeof collection === 'object'){
+      for(var key in collection){
+        iterator(collection[key], key, collection);
+      }
+    }
+  
   };
 
   // Returns the index at which value can be found in the array, or -1 if value
@@ -66,16 +84,59 @@
 
   // Return all elements of an array that pass a truth test.
   _.filter = function(collection, test) {
+    var result = [];
+    _.each(collection, function(ele, index){ 
+      if(test(ele)){
+        result.push(ele);
+      }
+    });
+    return result;
   };
 
   // Return all elements of an array that don't pass a truth test.
   _.reject = function(collection, test) {
     // TIP: see if you can re-use _.filter() here, without simply
     // copying code in and modifying it
+    var result = [];
+    _.each(collection, function(ele, index){ 
+      if(!test(ele)){
+        result.push(ele);
+      }
+    });
+    return result;
   };
 
   // Produce a duplicate-free version of the array.
   _.uniq = function(array, isSorted, iterator) {
+    var copyArray = array.slice();
+    var result = [];
+
+    if(iterator){
+      for(let ele of array){
+        result.push(iterator(ele));
+      }
+      copyArray = result;
+      result = [];
+    }
+
+    if(isSorted){
+      for(var i = 0; i < copyArray.length; i++){
+        if(i === 0){
+          result.push(array[i]);
+        }else if(copyArray[i - 1] !== copyArray[i]){
+          result.push(array[i]);
+        }
+      }
+      return result;
+    }
+
+    _.filter(copyArray, function(ele){
+      if(!result.includes(ele)){
+        result.push(ele);
+      }
+    });
+
+    return result;
   };
 
 
@@ -84,6 +145,12 @@
     // map() is a useful primitive iteration function that works a lot
     // like each(), but in addition to running the operation on all
     // the members, it also maintains an array of results.
+    var result = [];
+    var collCopy = collection.slice();
+    for(let ele of collCopy){
+      result.push(iterator(ele));
+    }
+    return result;
   };
 
   /*
@@ -92,7 +159,7 @@
    * as an example of this.
    */
 
-  // Takes an array of objects and returns and array of the values of
+  // Takes an array of objects and returns an array of the values of
   // a certain property in it. E.g. take an array of people and return
   // an array of just their ages
   _.pluck = function(collection, key) {
@@ -125,6 +192,18 @@
   //   }); // should be 5, regardless of the iterator function passed in
   //          No accumulator is given so the first element is used.
   _.reduce = function(collection, iterator, accumulator) {
+
+    let checkAccum = accumulator === undefined ? true : false;
+
+    accumulator = accumulator === undefined ? collection[0] : accumulator;
+
+    for(let i = checkAccum ? 1 : 0; i < collection.length; i++){
+      accumulator = iterator(accumulator, collection[i]);
+    }
+    
+    return accumulator;
+   
+   
   };
 
   // Determine if the array or object contains a given value (using `===`).
